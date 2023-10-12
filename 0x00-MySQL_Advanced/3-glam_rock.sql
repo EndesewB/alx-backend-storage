7c87385fcf7df1fffa5bf562aad63a414e2b8bf2
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-12 21:38:23 +0300

    glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,5 @@
 -- List Glam rock bands ranked by longevity
-SELECT
-    band_name,
-    IF(split > 0, 2022 - formed, 0) AS lifespan
-FROM
-    bands
-WHERE
-    main_style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
